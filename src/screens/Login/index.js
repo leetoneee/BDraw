03f143fed2e4627574b8a8a95f5c041682c3120b
Binkeurss <nvbinh271104@gmail.com>
2024-06-17 22:21:02 +0700

    fixLoginScreen

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,100 +1,125 @@
-import { Text, View, Button, Image, Animated, TouchableOpacity, TextInput } from "react-native";
+import {
+  Text,
+  View,
+  Button,
+  Image,
+  Animated,
+  TouchableOpacity,
+  TextInput,
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect, useRef, useState } from "react";
-import { decrement, increment } from "../../redux/counterSlice/counterSlice";
-import { Icon, MD3Colors } from 'react-native-paper';
-import Background from "../../components/Background";
-import styles from "./styles";
-import { height, width } from "../../constants";
+import {useDispatch, useSelector} from 'react-redux';
+import {useEffect, useRef, useState} from 'react';
+import {decrement, increment} from '../../redux/counterSlice/counterSlice';
+import {Icon, MD3Colors} from 'react-native-paper';
+import Background from '../../components/Background';
+import styles from './styles';
+import {height, width} from '../../constants';
 import axios from 'axios';
 import React from 'react';
 import LinearGradient from 'react-native-linear-gradient';
-import { Svg, Polygon } from 'react-native-svg';
+import {Svg, Polygon} from 'react-native-svg';
 import background_pen from '../../assets/images/background_pen.png';
 
 const FROM_COLOR = '#A541E1';
-const VIA_COLOR = '#8752E4'
+const VIA_COLOR = '#8752E4';
 const TO_COLOR = '#6F60E7';
 
-function Login({ navigation }) {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [rememberPassword, setRememberPassword] = useState(false);
+function Login({navigation}) {
+  console.log({height, width});
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [rememberPassword, setRememberPassword] = useState(false);
 
-    const animatedLogin = useRef(new Animated.Value(1000)).current;
-    const animatedBdraw = useRef(new Animated.ValueXY({ x: 300, y: 300, })).current;
+  const animatedLogin = useRef(new Animated.Value(1000)).current;
+  const animatedBdraw = useRef(new Animated.ValueXY({x: 300, y: 300})).current;
 
-    useEffect(() => {
-        Animated.timing(animatedLogin, {
-            toValue: height / 2.7,
-            duration: 1000,
-            useNativeDriver: false,
-        }).start();
+  useEffect(() => {
+    Animated.timing(animatedLogin, {
+      toValue: height / 2.7,
+      duration: 1000,
+      useNativeDriver: false,
+    }).start();
 
-        Animated.timing(animatedBdraw, {
-            toValue: { x: -70, y: -40 },
-            duration: 1000,
-            useNativeDriver: false,
-        }).start();
-    }, [animatedLogin, animatedBdraw]);
+    Animated.timing(animatedBdraw, {
+      toValue: {x: -70, y: -40},
+      duration: 1000,
+      useNativeDriver: false,
+    }).start();
+  }, [animatedLogin, animatedBdraw]);
 
-    return (
-        <View style={styles.container}>
-            <Animated.View style={{ marginTop: animatedBdraw.x, marginLeft: animatedBdraw.y }}>
-                <Image source={background_pen} resizeMode='contain' style={{ width: 500, height: 500, transform: [{ rotate: '-20deg' }] }} />
-            </Animated.View>
-            <LinearGradient
-                colors={['#A541E1', '#8752E4', '#6F60E7']}
-                style={styles.gradient}
-                start={{ x: 0, y: 0 }}
-                end={{ x: 1, y: 1 }}
-            />
-            <Animated.View style={{backgroundColor:'white', width: width /1.25, height: height, position: "absolute", alignSelf:'center', alignItems:'center', justifyContent:'flex-start', borderRadius: 15, paddingHorizontal:20, transform: [{ translateY: animatedLogin }]}}>
-                
-                <Text style={{ fontSize: 45, fontFamily: 'VampiroOne-Regular', color: '#9b23d0' }}>BDraw</Text>
-                
-                <TextInput
-                    style={styles.input}
-                    placeholder="Username"
-                    placeholderTextColor="#BD98D1"
-                    value={username}
-                    onChangeText={setUsername}
-                />
-                
-                <TextInput
-                    style={styles.input}
-                    placeholder="Password"
-                    placeholderTextColor="#BD98D1"
-                    secureTextEntry
-                    value={password}
-                    onChangeText={setPassword}
-                />
-                
-                <View style={styles.checkboxContainer}>
-                    <CheckBox
-                    value={rememberPassword}
-                    onValueChange={setRememberPassword}
-                    tintColors={{ true: '#6a0dad', false: '#aaa' }}
-                    />
-                    <Text style={styles.label}>Remember password</Text>
-                </View>
-                
-                <TouchableOpacity >
-                    <LinearGradient
-                    colors={[FROM_COLOR, VIA_COLOR, TO_COLOR]}
-                    style={styles.linearGradient}>
-                    <Text style={styles.textStyle}>Login</Text>
-                    </LinearGradient>
-                </TouchableOpacity>
-                
-                <TouchableOpacity>
-                    <Text style={styles.forgotPassword}>Forgot your password?</Text>
-                </TouchableOpacity>
-            </Animated.View>
-
+  return (
+    <SafeAreaView style={styles.container}>
+      <Animated.View
+        style={{marginTop: animatedBdraw.x, marginLeft: animatedBdraw.y}}>
+        <Image
+          source={background_pen}
+          resizeMode="contain"
+          style={styles.background_pen}
+        />
+      </Animated.View>
+      <LinearGradient
+        colors={['#A541E1', '#8752E4', '#6F60E7']}
+        style={styles.gradient}
+        start={{x: 0, y: 0}}
+        end={{x: 1, y: 1}}
+      />
+      <Animated.View
+        style={[
+          styles.animatedView,
+          {transform: [{translateY: animatedLogin}]},
+        ]}>
+        <View style={styles.TextBDrawContainer}>
+          <Text style={styles.textBdraw}>BDraw</Text>
         </View>
-    );
+        <View style={styles.username_password_Input}>
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+            placeholderTextColor="#BD98D1"
+            value={username}
+            onChangeText={setUsername}
+          />
+        </View>
+        <View style={styles.username_password_Input}>
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            placeholderTextColor="#BD98D1"
+            secureTextEntry
+            value={password}
+            onChangeText={setPassword}
+          />
+        </View>
+
+        <View style={styles.checkboxContainer}>
+          <CheckBox
+            value={rememberPassword}
+            onValueChange={setRememberPassword}
+            tintColors={{true: '#6a0dad', false: '#aaa'}}
+          />
+          <Text style={styles.label}>Remember password</Text>
+        </View>
+
+        <View style={styles.loginContainer}>
+          <TouchableOpacity>
+            <LinearGradient
+              colors={[FROM_COLOR, VIA_COLOR, TO_COLOR]}
+              style={styles.linearGradient}>
+              <Text style={styles.textStyle}>Login</Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.forgotPasswordContainer}>
+          <TouchableOpacity>
+            <Text style={styles.forgotPassword}>Forgot your password?</Text>
+          </TouchableOpacity>
+        </View>
+      </Animated.View>
+    </SafeAreaView>
+  );
 }
 
-export default Login
+export default Login;
--- src/screens/Login/styles.js
+++ src/screens/Login/styles.js
@@ -1,61 +1,94 @@
-import { StyleSheet, Dimensions } from "react-native";
-import { height, width } from "../../constants";
+import {StyleSheet, Dimensions, Animated} from 'react-native';
+import {height, width} from '../../constants';
 
 export default styles = StyleSheet.create({
-    container: {
-        flex: 1,
-      },
+  container: {
+    flex: 1,
+  },
 
-      imagebackground: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginVertical: 30,
+  imagebackground: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 30,
+  },
 
-      },
+  gradient: {
+    width: width,
+    height: height,
+    top: height / 2.5,
+    position: 'absolute',
+  },
 
-    gradient: {
-        width: width,
-        height: height,
-        top: height / 2.5,
-        position: 'absolute',
-      },
-
-      input: {
-        width: '100%',
-        borderColor: '#6a0dad',
-        borderWidth: 1,
-        borderRadius: 5,
-        paddingHorizontal: 10,
-        marginHorizontal: 20,
-        marginBottom: 20,
-        fontSize: 18,
-      },
-      checkboxContainer: {
-        flexDirection: 'row',
-        marginBottom: 20,
-        alignItems: 'center',
-        alignSelf: "flex-start"
-      },
-      label: {
-        fontSize: 18,
-        fontWeight: "600",
-        color:'black',
-      },
-      linearGradient: {
-        borderRadius: 50,
-        margin: 10
-      },
-      textStyle: {
-        color: 'white',
-        fontWeight: '500',
-        textAlign: 'center',
-        fontSize: 35,
-        marginHorizontal: 50,
-        marginVertical: 5,
-      },
-      forgotPassword: {
-        color: '#6a0dad',
-        fontSize: 16,
-      },
+  input: {
+    width: '100%',
+    borderColor: '#6a0dad',
+    borderWidth: 1,
+    borderRadius: 5,
+    fontSize: 18,
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+    alignItems: 'center',
+    alignSelf: 'flex-start',
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: 'black',
+  },
+  linearGradient: {
+    borderRadius: 50,
+    margin: 10,
+  },
+  loginContainer: {
+    marginBottom: 20
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: '500',
+    textAlign: 'center',
+    fontSize: 35,
+    marginHorizontal: 50,
+    marginVertical: 5,
+  },
+  forgotPassword: {
+    color: '#6a0dad',
+    fontSize: 16,
+  },
+  TextBDrawContainer: {
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  textBdraw: {
+    fontSize: 45,
+    fontFamily: 'VampiroOne-Regular',
+    color: '#9b23d0',
+  },
+  animatedView: {
+    backgroundColor: 'white',
+    width: width / 1.1,
+    height: height,
+    position: 'absolute',
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    borderRadius: 15,
+    paddingHorizontal: 20,
+    borderWidth: 2,
+  },
+  background_pen: {
+    width: width/0.8228571428571428,
+    height: height/1.780571428571429,
+    transform: [{rotate: '-20deg'}],
+  },
+  username_password_Input: {
+    width: '100%',
+    marginHorizontal: 20,
+    marginBottom: 20,
+  },
+  forgotPasswordContainer: {
+    marginBottom: 20,
+  }
 });
